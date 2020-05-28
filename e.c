7c7828e7779baccb5564b22c85cc31b649858077
Andriy Begma <abegma@c1r9p8.kh.unitfactory.ua>
2020-05-28 17:39:37 +0300

    v0.0.2

---
+++ e.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int my_strlen(char b[99]);
+int my_atoi(char c[99]);
+int main()
+{
+  char a[99];
+  scanf("%s",a);
+  int kol = my_strlen(a);
+  int d = my_atoi(a);
+  printf("%d\n",kol);
+  printf("%d\n",d);
+  return 0;
+}
+int my_strlen(char b[99])
+{
+  int col,i=0;
+  while (b[i]!='\0')
+   {
+     col++;
+     i++;
+   }
+   return col;
+}
+int my_atoi(char b[99])
+{
+  int d=0;
+  if(b[0]!='\0')
+    {
+      int k=0;
+      while(b[k]!='\0')
+      {
+        if (b[k]=='1'){d=d*10+1;}
+        if (b[k]=='2'){d=d*10+2;}
+        if (b[k]=='3'){d=d*10+3;}
+        if (b[k]=='4'){d=d*10+4;}
+        if (b[k]=='5'){d=d*10+5;}
+        if (b[k]=='6'){d=d*10+6;}
+        if (b[k]=='7'){d=d*10+7;}
+        if (b[k]=='8'){d=d*10+8;}
+        if (b[k]=='9'){d=d*10+9;}
+        k++;
+      }
+    }
+    return d;
+}
